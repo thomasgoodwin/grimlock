4b761c8f03a95e8e053a15ab135a5bde1a2d92cd
thomas.goodwin <thomas@bittnerfamily.net>
2023-12-13 13:11:24 -0800

    glfw window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 
-int main(void) {
-	std::cout << "yes";
-	return 0;
+#include "GL/glew.h"
+#include "glfw/glfw3.h"
+
+int main(void)
+{
+  if (!glfwInit())
+  {
+    std::cerr << "glfw init failed" << std::endl;
+    return -1;
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+  GLFWwindow *window = glfwCreateWindow(1280, 720, "Grimlock", nullptr, nullptr);
+  if (window == nullptr)
+  {
+    std::cerr << "GLFW window creation failed." << std::endl;
+    glfwTerminate();
+    return -2;
+  }
+  glfwMakeContextCurrent(window);
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK)
+  {
+    std::cout << "GLEW init failed." << std::endl;
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return -3;
+  }
+
+  glClearColor(0.3f, 0.3f, 0.4f, 1.0f);
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glfwSwapBuffers(window);
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
 }
--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-"C:\Program Files\CMake\bin\cmake.EXE" --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\msys64\ucrt64\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\msys64\ucrt64\bin\g++.exe -SE:/freelance/cmake-test -Be:/freelance/cmake-test/build -G "MinGW Makefiles"
-"C:\Program Files\CMake\bin\cmake.EXE" --build e:/freelance/cmake-test/build --config Debug --target all -j 14 --
+"C:\Program Files\CMake\bin\cmake.EXE" --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\msys64\ucrt64\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\msys64\ucrt64\bin\g++.exe -SE:/freelance/grimlock -Be:/freelance/grimlock/build -G "MinGW Makefiles"
+"C:\Program Files\CMake\bin\cmake.EXE" --build e:/freelance/grimlock/build --config Debug --target all -j 14 --
 cd .\build\
 .\Grimlock.exe
