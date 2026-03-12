a22b77472ec1d8e1e837c4c47fa05be928cf38c5
thomas.goodwin <thomas@bittnerfamily.net>
2026-03-12 00:41:00 -0700

    remove examples

--- fmod/api/core/examples/common_platform.h
+++
@@ -1,6 +0,0 @@
-/*==============================================================================
-FMOD Example Framework
-Copyright (c), Firelight Technologies Pty, Ltd 2013-2026.
-==============================================================================*/
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
